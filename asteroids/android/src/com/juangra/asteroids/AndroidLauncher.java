6f080e5314dfee0d3eece01f8a25afcb7323e980
Iván Carrillo <nawtovi@gmail.com>
2017-01-24 11:56:56 +0100

    Jodienda dos

--- asteroids/android/src/com/juangra/asteroids/AndroidLauncher.java
+++ asteroids/android/src/com/juangra/asteroids/AndroidLauncher.java
@@ -1,5 +1,7 @@
 package com.juangra.asteroids;
 
+import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.juangra.asteroids.MyGdxGame;

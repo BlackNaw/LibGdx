9e815553b4f81ee469e63083dc84c3957704f88b
Iván Carrillo <nawtovi@gmail.com>
2017-02-04 14:00:38 +0100

    Cambiuos

--- PreAngryBirds/core/src/com/ivan/ManyBalls.java
+++ PreAngryBirds/core/src/com/ivan/ManyBalls.java
@@ -12,9 +12,10 @@ import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class ManyBalls extends ApplicationAdapter {
-	private static final int NUMBER_OF_BALLS = 100;
+	private static final int NUMBER_OF_BALLS = 60;
 	private static final float GRAVEDAD_Y = -90f;
 	public static final int FactorZoomCamara = 2;
 	SpriteBatch batch;
@@ -26,6 +27,7 @@ public class ManyBalls extends ApplicationAdapter {
 	OrthographicCamera camera;
 	ArrayList<Ball> balls;
 	ArrayList<Ball> deathBalls;
+	ArrayList<Ground> tablas;
 	Ground ground;
 
 	@Override
@@ -36,7 +38,12 @@ public class ManyBalls extends ApplicationAdapter {
 		camera = new OrthographicCamera(Gdx.graphics.getWidth() * FactorZoomCamara,
 				Gdx.graphics.getHeight() * FactorZoomCamara);
 		balls = new ArrayList<Ball>();
-		ground = new Ground(world, 0, -2.4f, 300, 0);
+		tablas = new ArrayList<Ground>();
+		ground = new Ground(world, -Gdx.graphics.getWidth() / 2, -Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth(),
+				50, BodyType.StaticBody);
+		tablas.add(new Ground(world, 150, -Gdx.graphics.getHeight() / 2 + 100, 5, 50, BodyType.DynamicBody));
+		tablas.add(new Ground(world, 250, -Gdx.graphics.getHeight() / 2 + 100, 5, 50, BodyType.DynamicBody));
+		tablas.add(new Ground(world, 200, -Gdx.graphics.getHeight() / 2 + 155, 55, 5, BodyType.DynamicBody));
 	}
 
 	@Override
@@ -50,9 +57,14 @@ public class ManyBalls extends ApplicationAdapter {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		debugMatrix = batch.getProjectionMatrix().cpy().scale(Utiles.PIXELS_TO_METERS, Utiles.PIXELS_TO_METERS, 0);
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		for (Ground tabla : tablas) {
+			tabla.update();
+//			tabla.draw(batch);
+		}
+		ground.draw(batch);
 		for (Iterator iterator = balls.iterator(); iterator.hasNext();) {
 			Ball ball = (Ball) iterator.next();
 			ball.update();
@@ -61,7 +73,7 @@ public class ManyBalls extends ApplicationAdapter {
 				iterator.remove();
 		}
 		batch.end();
-		// debugRenderer.render(world, debugMatrix);
+		debugRenderer.render(world, debugMatrix);
 	}
 
 	@Override

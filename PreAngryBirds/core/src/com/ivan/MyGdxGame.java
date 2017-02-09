92ab7e239d684493e5ff4871265b34e292b19b4b
Iván Carrillo <nawtovi@gmail.com>
2017-02-09 10:13:18 +0100

    Fisicas y box

--- PreAngryBirds/core/src/com/ivan/MyGdxGame.java
+++ PreAngryBirds/core/src/com/ivan/MyGdxGame.java
@@ -10,14 +10,14 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private static final int NUMBER_OF_BALLS = 60;
-	private static final float GRAVEDAD_Y = -90f;
-	public static final int FactorZoomCamara = 3;
+	private static final int NUMBER_OF_BALLS = 1;
+	private static final float GRAVEDAD_Y = -9.8f;
+	public static final float FactorZoomCamara = 1f;
 	SpriteBatch batch;
 	World world;
 	float timeElapsed = 0, timeLapse = .0f;
@@ -25,34 +25,32 @@ public class MyGdxGame extends ApplicationAdapter {
 	Box2DDebugRenderer debugRenderer;
 	Matrix4 debugMatrix;
 	OrthographicCamera camera;
-	ArrayList<Ball> balls;
-	ArrayList<Ball> deathBalls;
+	Ball ball;
 	ArrayList<Ground> tablas;
-	Ground ground;
+	Ground suelo,paredDer,paredIzq,techo;
 
 	@Override
 	public void create() {
+		tablas=new ArrayList<Ground>();
 		batch = new SpriteBatch();
 		world = new World(new Vector2(0, GRAVEDAD_Y), true);
+		ball = new Ball(world);
 		debugRenderer = new Box2DDebugRenderer();
-		camera = new OrthographicCamera(Gdx.graphics.getWidth() * FactorZoomCamara,
-				Gdx.graphics.getHeight() * FactorZoomCamara);
-		balls = new ArrayList<Ball>();
-		tablas = new ArrayList<Ground>();
-		ground = new Ground(world, -Gdx.graphics.getWidth() / 2, -Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth(),
-				50, BodyType.StaticBody);
-		tablas.add(new Ground(world, 150, -Gdx.graphics.getHeight() / 2 + 100, 5, 50, BodyType.DynamicBody));
-		tablas.add(new Ground(world, 250, -Gdx.graphics.getHeight() / 2 + 100, 5, 50, BodyType.DynamicBody));
-		tablas.add(new Ground(world, 200, -Gdx.graphics.getHeight() / 2 + 155, 55, 5, BodyType.DynamicBody));
+		camera = new OrthographicCamera(Gdx.graphics.getWidth() * FactorZoomCamara, Gdx.graphics.getHeight()
+				* FactorZoomCamara);
+		suelo=new Ground(world,-camera.viewportWidth/4, -camera.viewportHeight/2,camera.viewportWidth, 50,BodyType.StaticBody);
+		paredDer=new Ground(world,camera.viewportWidth/2+10, 0,10,camera.viewportHeight,BodyType.StaticBody);
+		paredIzq=new Ground(world,-camera.viewportWidth/2-10, 0,10,camera.viewportHeight,BodyType.StaticBody);
+		techo=new Ground(world,-camera.viewportWidth/4, camera.viewportHeight/2+10,camera.viewportWidth, 10,BodyType.StaticBody);
+		tablas.add(new Ground(world, 150, -Gdx.graphics.getHeight()/2+100, 5, 50,BodyType.DynamicBody));
+		tablas.add(new Ground(world, 250, -Gdx.graphics.getHeight()/2+100, 5, 50,BodyType.DynamicBody));
+		tablas.add(new Ground(world,200, -Gdx.graphics.getHeight()/2+155, 55, 5,BodyType.DynamicBody));
+		Gdx.input.setInputProcessor(ball);
 	}
 
 	@Override
 	public void render() {
 		timeElapsed += Gdx.graphics.getDeltaTime();
-		if (balls.size() < NUMBER_OF_BALLS && timeElapsed > timeLapse) {
-			timeElapsed = 0;
-			balls.add(new Ball(world));
-		}
 		world.step(1f / 60f, 6, 2);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
@@ -60,20 +58,19 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		//Al sacar la iteracion de la estructura de arraylist ya
+		//podemos modificar el arraylist porque no es el encargado de recorrerlo
+		ball.update();
+		ball.draw(batch);
+		suelo.draw(batch);
 		for (Ground tabla : tablas) {
 			tabla.update();
 			tabla.draw(batch);
 		}
-		ground.draw(batch);
-		for (Iterator iterator = balls.iterator(); iterator.hasNext();) {
-			Ball ball = (Ball) iterator.next();
-			ball.update();
-			ball.draw(batch);
-			if (ball.isDead())
-				iterator.remove();
-		}
+	
+		
 		batch.end();
-		debugRenderer.render(world, debugMatrix);
+//		debugRenderer.render(world, debugMatrix);
 	}
 
 	@Override

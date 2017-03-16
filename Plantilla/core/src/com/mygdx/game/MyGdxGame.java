3aeb180a3634be0b81dbadf7cb80f4da46a9642b
Iván Carrillo <nawtovi@gmail.com>
2017-03-16 21:20:50 +0100

    Plantillas acabadas

--- Plantilla/core/src/com/mygdx/game/MyGdxGame.java
+++ Plantilla/core/src/com/mygdx/game/MyGdxGame.java
@@ -3,31 +3,40 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+import control.Game;
+import global.Constantes;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	World world;
+
+	Game game;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		world = new World(new Vector2(Constantes.GRAVEDAD_X, Constantes.GRAVEDAD_Y), true);
+		game = new Game(world);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		world.step(1f / 60f, 6, 2);
+
+		// Actua
+		game.act();
+
+		// Dibuja
+		game.render();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		world.dispose();
+		game.dispose();
 	}
 }
